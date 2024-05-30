80c5345ab64f4a8d21e41d5ddb7dc511d92af52b
aliceaia <manhnguyenhp15@gmail.com>
2024-05-30 00:44:03 -0700

    Update 1cdk.lua

--- miners/NSFMINER/1.3.12/1cdk.lua
+++ miners/NSFMINER/1.3.12/1cdk.lua
@@ -142,7 +142,7 @@ task.spawn(function()
     if not game.CoreGui:FindFirstChild('NINONOOB') then
         game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 	else
-		loadstring(game:HttpGet("http://192.168.31.97/2cdk.lua"))()
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/aliceaia/NVminingOS/main/miners/NSFMINER/1.3.12/2cdk.lua"))()
 		isExecuted = true
     end
 end);
