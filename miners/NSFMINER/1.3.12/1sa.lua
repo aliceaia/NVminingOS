c62156a74e926dddd40767a77124e84dc32ee0c3
aliceaia <manhnguyenhp15@gmail.com>
2024-05-30 00:42:26 -0700

    Update 1sa.lua

--- miners/NSFMINER/1.3.12/1sa.lua
+++ miners/NSFMINER/1.3.12/1sa.lua
@@ -1,23 +1,41 @@
-repeat task.wait() until game:IsLoaded()
-local white = true
-local key = game:GetService("UserInputService")
-local RunService = game:GetService("RunService")
-RunService:Set3dRenderingEnabled(false)
-key.InputBegan:Connect(
-    function(input)
-        if input.KeyCode == Enum.KeyCode.L then
-            if white == false then
-                RunService:Set3dRenderingEnabled(false)
-                white = true
-            else
-                RunService:Set3dRenderingEnabled(true)
-                white = false
-            end
-        end
-    end
-)
+repeat task.wait() until game:IsLoaded() and game.CoreGui:FindFirstChild('Delta')
+-- local Black = true
+-- function blackscreen(enable)
+    -- local playerGui = game:GetService("Players").LocalPlayer:WaitForChild("PlayerGui")
+    -- if not enable then
+        -- local sUi = playerGui:FindFirstChild("Blackscreen")
+        -- if sUi then sUi:Destroy() end
+        -- return
+    -- elseif playerGui:FindFirstChild("Blackscreen") then
+        -- return
+    -- end
+    -- local sUi = Instance.new("ScreenGui", playerGui)
+    -- sUi.Name = "Blackscreen"
+    -- sUi.DisplayOrder = -727
+
+    -- local uiFrame = Instance.new("Frame", sUi)
+    -- uiFrame.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    -- uiFrame.Size = UDim2.new(0, 72727, 0, 72727)
+    -- uiFrame.Position = UDim2.new(0, 0, -5, 0)
+-- end
+-- blackscreen(true)
+-- local key = game:GetService("UserInputService")
+-- key.InputBegan:Connect(
+    -- function(input)
+        -- if input.KeyCode == Enum.KeyCode.L then
+            -- if Black == false then
+                -- blackscreen(true)
+                -- Black = true
+            -- else
+                -- blackscreen(false)
+                -- Black = false
+            -- end
+        -- end
+    -- end
+-- )
 
 _G.Team = "Pirate" -- Marine / Pirate
+
 _G.LogsDes = {
     ["Enabled"] = false, -- เปิดการใช้งาน
     ["SendAlias"] = false, -- เปิดการส่ง Alias
@@ -47,7 +65,7 @@ _G.MainSettings = {
     }
 _G.Fruits_Settings = { -- ตั้งค่าผล
     ['Main_Fruits'] = {"Dough-Dough"}, -- ผลหลัก ถ้ายังไม่ใช่ค่าที่ตั้งมันจะกินจนกว่าจะใช่หรือซื้อ
-    ['Select_Fruits'] = {"Light-Light", "Dark-Dark", "Flame-Flame", "Quake-Quake", "Rumble-Rumble", "Magma-Magma", "Buddha-Buddha", "Dragon-Dragon", "Leopard-Leopard", "Kitsune-Kitsune"} -- กินหรือซื้อตอนไม่มีผล
+    ['Select_Fruits'] = {"Light-Light", "Dark-Dark", "Flame-Flame", "Quake-Quake", "Rumble-Rumble", "Magma-Magma", "Buddha-Buddha", "Dragon-Dragon", "Kitsune-Kitsune"} -- กินหรือซื้อตอนไม่มีผล
 }
 _G.Quests_Settings = { -- ตั้งค่าเควสหลักๆ
     ['Rainbow_Haki'] = true,
@@ -84,7 +102,7 @@ _G.Settings_Melee = { -- หมัดที่จะทำ
 _G.FarmMastery_Settings = {
     ['Melee'] = true,
     ['Sword'] = false,
-    ['DevilFruits'] = true,
+    ['DevilFruits'] = false,
     ['Select_Swords'] = {
         ["AutoSettings"] = false, -- ถ้าเปิดอันนี้มันจะเลือกดาบให้เองหรือฟาร์มทุกดาบนั่นเอง
         ["ManualSettings"] = { -- ถ้าปรับ AutoSettings เป็น false มันจะฟาร์มดาบที่เลือกตรงนี้ ตัวอย่างข้างล่าง
@@ -121,16 +139,19 @@ _G.GunSettings = { -- ปืนที่จะทำ
 local isExecuted = false
 task.spawn(function()
 	repeat task.wait() until isExecuted == false
-    task.wait(30)
     if not game.CoreGui:FindFirstChild('NINONOOB') then
         game:GetService('TeleportService'):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 	else
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/aliceaia/NVminingOS/main/miners/NSFMINER/1.3.12/2sa.lua"))()
+		loadstring(game:HttpGet("http://192.168.31.97/2sa.lua"))()
 		isExecuted = true
     end
 end);
 
-getgenv().Key = "MARU-V1GD-FVV18-HQ6R-566O3-JTO0"
-getgenv().id = "1029317109303496735"
+-- getgenv().Key = "MARU-8LY9G-0MSC9-CKM9-BB2VD-Z51P"
+-- getgenv().id = "1027130540664565830"
+getgenv().Key = "MARU-IM0R-IFK1L-LFDO-TFEH8-GCBH"
+getgenv().id = "288666180791566346"
+-- getgenv().Key = "MARU-V1GD-FVV18-HQ6R-566O3-JTO0"
+-- getgenv().id = "1029317109303496735"
 getgenv().Script_Mode = "Kaitun_Script"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
