9bb3052119752947523f53a1abe6388129a7a09a
aliceaia <manhnguyenhp15@gmail.com>
2024-05-30 04:15:39 -0700

    Update 2sa.lua

--- miners/NSFMINER/1.3.12/2sa.lua
+++ miners/NSFMINER/1.3.12/2sa.lua
@@ -1,51 +1,4 @@
---task.wait(15)
--- local white = true
--- local key = game:GetService("UserInputService")
--- local RunService = game:GetService("RunService")
--- RunService:Set3dRenderingEnabled(false)
--- key.InputBegan:Connect(
-    -- function(input)
-        -- if input.KeyCode == Enum.KeyCode.L then
-            -- if white == false then
-                -- RunService:Set3dRenderingEnabled(false)
-                -- white = true
-            -- else
-                -- RunService:Set3dRenderingEnabled(true)
-                -- white = false
-            -- end
-        -- end
-    -- end
--- )
-
 repeat task.wait() until game.CoreGui:FindFirstChild('NINONOOB')
--- task.wait(5)
-
--- local UGS = UserSettings():GetService'UserGameSettings'
--- UGS.MasterVolume = 0
--- UGS.SavedQualityLevel = 1
--- for k,v in pairs(game:GetService("CoreGui"):GetChildren()) do
-    -- if v.Name ~= "RobloxGui" and v.Name ~= "CoreScriptLocalization" and v.Name ~= "TeleportGui" and v.Name ~= "RobloxPromptGui" and
-        -- v.Name ~= "OnRootedListener" and v.Name ~= "CursorContainer" and v.Name ~= "PlayerList" and v.Name ~= "ScreenshotsCarousel" and v.Name ~= "CaptureOverlay" and
-        -- v.Name ~= "RobloxNetworkPauseNotification" and v.Name ~= "ToastNotification" and v.Name ~= "TeleportEffectGui" and v.Name ~= "SocialContextToast" and v.Name ~= "GameInvite" and
-        -- v.Name ~= "PurchasePrompt" and v.Name ~= "PublishAssetPrompt" and v.Name ~= "ThemeProvider" and v.Name ~= "AdsEuDsaPromptHandler" and v.Name ~= "CallDialogScreen" and v.Name ~= "PlayerMenuScreen" and
-        -- v.Name ~= "ContactList" and v.Name ~= "HeadsetDisconnectedDialog" and v.Name ~= "DevConsoleMaster" and v.Name ~= "drawingDirectory" and v.Name ~= "NINONOOB" and v.Name ~= "AkaliNotif" then
-        -- v.Enabled = false
-    -- end
--- end
-
--- game:GetService("Players").LocalPlayer.PlayerGui.TopbarPlus.Enabled = false
--- game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.Chat, false)
--- game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.PlayerList, false)
--- game:GetService("GuiService").TouchControlsEnabled = false
--- game:GetService("Players").LocalPlayer.PlayerGui.Main.Compass.Visible = false
--- game:GetService("Players").LocalPlayer.PlayerGui.Main.DynamicTopBar.Visible = false
--- game:GetService("Players").LocalPlayer.PlayerGui.Main.AlliesButton.Visible = false
--- game:GetService("Players").LocalPlayer.PlayerGui.Main.Code.Visible = false
--- game:GetService("Players").LocalPlayer.PlayerGui.Main.CrewButton.Visible = false
--- game:GetService("Players").LocalPlayer.PlayerGui.Main.HomeButton.Visible = false
--- game:GetService("Players").LocalPlayer.PlayerGui.Main.Mute.Visible = false
--- game:GetService("Players").LocalPlayer.PlayerGui.Main.Settings.Visible = false
--- gethui():FindFirstChild("FluxusAndroidUI").Enabled = false
 if game.CoreGui:FindFirstChild('FluxusAndroidUI') then
 	game.CoreGui:FindFirstChild('FluxusAndroidUI').Enabled = false
 end
@@ -67,17 +20,6 @@ key.InputBegan:Connect(
         end
     end
 )
--- if game.CoreGui:FindFirstChild("TrigonTopbar")then
-	-- for r, v in pairs(
-		-- getconnections(
-			-- game:GetService("CoreGui").TrigonTopbar.TopbarFrame.Left.Trigon.IconButton.Activated
-		-- )
-	-- ) do
-		-- v.Function()
-	-- end
-	-- game:GetService("CoreGui").TrigonTopbar:Destroy()
--- end
-
 
 local Player = game:GetService("Players").LocalPlayer.Name
 local UID = game:GetService("Players").LocalPlayer.UserId
